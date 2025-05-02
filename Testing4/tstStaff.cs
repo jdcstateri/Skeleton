f9e1689aacca271297aabb5a2888b57e49c95699
KDIS11 <inoshsanjaya2003@gmail.com>
2025-05-02 12:34:12 +0100

    Added Testing for Staff Class

---
+++ Testing4/tstStaff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create a instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+
+            //Test to see that is Exist
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
