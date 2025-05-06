1e6352465f3d2db0faf5e75388282e16ca58b7e3
ev-dlks <evandilkes2@gmail.com>
2025-05-06 16:56:22 +0100

    changed clsOrder and clsOrderLine to use private properties with public accessors

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,18 +1,45 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ClassLibrary
 {
     public class clsOrder
     {
+        private int OrderId;
+        private int AccountId;
+        private float TotalCost;
+        private DateTime DateOfDelivery;
+        private bool Delivered;
+        private string DeliveryInstructions;
+
         public clsOrder()
         {
         }
 
-        public int OrderId { get; set; }
-        public int AccountId { get; set; }
-        public float TotalCost { get; set; }
-        public DateTime DateOfDelivery { get; set; }
-        public bool Delivered { get; set; }
-        public string DeliveryInstructions { get; set; }
+        // getters
+        public int GetOrderId() { return this.OrderId; }
+
+        public int GetAccountId() { return this.AccountId; }
+
+        public float GetTotalCost() {return this.TotalCost; }
+
+        public DateTime GetDateOfDelivery(){ return this.DateOfDelivery; }
+
+        public bool GetDelivered(){ return this.Delivered; }
+
+        public string GetDeliveryInstructions(){ return this.DeliveryInstructions; }
+
+        // setters
+        public void SetOrderId(int id){ this.OrderId = id; }
+
+        public void SetAccountId(int accountId){ this.AccountId = accountId; }
+
+        public void SetTotalCost(float totalCost){ this.TotalCost = totalCost; }
+
+        public void SetDateOfDelivery(DateTime dateOfDelivery) { this.DateOfDelivery = dateOfDelivery; }
+
+        public void SetDelivered(bool delivered){ this.Delivered = delivered; }
+
+        public void SetDeliveryInstructions(string deliveryInstructions) { this.DeliveryInstructions = deliveryInstructions; } 
     }
 }
