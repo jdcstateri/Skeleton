6a1190b509c396e274fe0512019f49ebe73af5c8
KDIS11 <inoshsanjaya2003@gmail.com>
2025-05-16 13:44:32 +0100

    Implemented StaffCollection class along with relevant test. Add data layer as well to filter out the date from the database using stored procedure. Then added presentation layer to staffList page to show all the staff names which is available in the database.
    Made some changes to staffDataEntry page as well.

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(IsPostBack == false)
+        {
+            //update the listbox
+            DisplayStaff();
+        }
+    }
 
+    void DisplayStaff()
+    {
+        //create an instance of the Staff Collection
+        clsStaffColletion Staff = new clsStaffColletion();
+
+        //set the data source to the list staff in the collectioj
+        lstStaffList.DataSource = Staff.StaffList;
+
+        //set the name of the primary key
+        lstStaffList.DataValueField = "StaffID";
+
+        //Set the data field to display
+        lstStaffList.DataValueField = "Name";
+
+        //Bind the data to the list
+        lstStaffList.DataBind();
     }
 }
